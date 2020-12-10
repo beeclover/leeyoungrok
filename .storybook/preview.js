23b74a43a900b2d539bcdc01daae1f3f73a4843d
Hansanghyeon <dandan9509@gmail.com>
2020-12-10 19:34:02 +0900

    :wrench: storybook 전역 스타일추가, atomic 폴더에서 스토리까지

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,31 @@
 import { addDecorator } from '@storybook/react';
+import { createGlobalStyle } from 'styled-components';
 import { withThemesProvider } from 'themeprovider-storybook';
 import DefaultTheme from '@style/Theme';
 const themes = [DefaultTheme];
+
+const GlobalStyle = createGlobalStyle`
+  html, body, #root {
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+  .sbdocs-wrapper {
+    min-height: 100%;
+    box-sizing: border-box;
+  }
+  * {
+    outline: none;
+    /* font-family: 'Noto Sans','Noto Sans KR', sans-serif; */
+    box-sizing: border-box;
+  }
+`;
+
+addDecorator((story) => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
+
 addDecorator(withThemesProvider(themes));
