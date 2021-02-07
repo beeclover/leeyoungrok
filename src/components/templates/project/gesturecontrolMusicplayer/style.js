07d70595c6852a101e2a70eb9f8cfe152a148964
Hansanghyeon <999@hyeon.pro>
2021-02-07 23:01:29 +0900

    :recycle: Optimise

--- src/components/templates/project/gesturecontrolMusicplayer/style.js
+++ src/components/templates/project/gesturecontrolMusicplayer/style.js
@@ -1,47 +1,47 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 export const Custom = styled.div`
-.main_wrap{
+  .main_wrap {
     width: 1500px;
     margin: 0 auto;
-}
-.mainvideo {
+  }
+  .mainvideo {
     width: 100%;
     height: 720px;
     text-align: center;
-}
-.mainvideo video{
+  }
+  .mainvideo video {
     width: auto;
     height: 100%;
-}
-.project_title{
+  }
+  .project_title {
     font-size: 40px;
     font-weight: bold;
-}
-.project_overview{
+  }
+  .project_overview {
     font-size: 16;
-}
-.content_head{
+  }
+  .content_head {
     font-size: 50px;
     font-weight: bolder;
-}
-.LTC_images img{
+  }
+  .LTC_images img {
     width: 1500px;
     height: auto;
     display: block;
-}
+  }
 
-.LTC_images_wrap{
+  .LTC_images_wrap {
     text-align: center;
-}
-.video_head {
+  }
+  .video_head {
     font-size: 40px;
     font-weight: bolder;
-}
-.video_content{
+  }
+  .video_content {
     font-size: 16px;
-}
-.video_clip{
+  }
+  .video_clip {
     height: auto;
     width: 300px;
-}
-`
+  }
+`;
--- src/pages/project/gesturecontrol_musicplayer.js
+++ src/pages/project/gesturecontrol_musicplayer.js
@@ -1,4 +1,4 @@
-import GesturecontrolMusicpalyer from "@component/templates/project/gesturecontrolMusicplayer"
-export default function() {
-    return <GesturecontrolMusicpalyer />
+import GesturecontrolMusicpalyer from '@component/templates/project/gesturecontrolMusicplayer';
+export default function () {
+  return <GesturecontrolMusicpalyer />;
 }
